2349607a5d1699e19866883bdace9034cc0a28b6
jie.hua <jiehua@jiehua.local>
2015-04-16 20:03:38 +0800

    =jdk15

---
+++ jdk15demo/src/main/java/com/demo/sdk/util/AlipayClientUtil.java
@@ -0,0 +1,17 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2015 All Rights Reserved.
+ */
+package com.demo.sdk.util;
+
+/**
+ * 
+ * @author jiehua
+ * @version $Id: AlipayClientUtil.java, v 0.1 2015-4-16 обнГ8:02:38 jiehua Exp $
+ */
+public class AlipayClientUtil {
+    
+    
+    private static final AlipayClient
+
+}
